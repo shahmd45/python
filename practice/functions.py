16ce15d97757b411027d4d4df70c34ea91e59b9b
MD Shaheb Hussain <shahmd45@gmail.com>
2026-08-11 10:27:07 +0530

    functions in python

---
+++ practice/functions.py
@@ -0,0 +1,25 @@
+# calculate sum of 2 numbers
+
+'''
+def sum(a, b):
+    return a + b
+
+ans = sum(3,4)
+print(ans)
+'''
+
+
+# default parameters
+
+def sum (a = 0, b = 0):
+    return a + b
+
+ans = sum(True)
+print(ans)
+
+
+'''
+    2 types of function in python
+        1. buit-in function
+        2. user-defined function
+'''
